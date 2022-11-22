405401e234f219fb449ee27716f6d0382b215775
Lukasz Wolny <lukasz360ig@gmail.com>
2022-11-22 19:38:46 +0100

    added hover styles

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const menuBtn = document.querySelector('.menu-btn')
-const mobileMenu = document.querySelector('.mobile-menu')
+const menuBtn = document.querySelector(".menu-btn")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 const toggleMobileMenu = () => {
-    mobileMenu.classList.toggle('hidden')
+  mobileMenu.classList.toggle("hidden")
 }
 
-menuBtn.addEventListener('click', toggleMobileMenu)
-
+menuBtn.addEventListener("click", toggleMobileMenu)
